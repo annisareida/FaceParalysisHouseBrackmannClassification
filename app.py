91cb36c664af709eb55b8dac39cd829c34e26f25
annisareida <139696700+annisareida@users.noreply.github.com>
2025-07-05 20:13:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 # Load model
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model('final_modelYFPMEIIBINER100modelfit2.h5', compile=False)
+    return tf.keras.models.load_model('final_modelYFPMEIIBINERaug300.h5')
 
 model = load_model()
 class_names = ['Normal', 'Paralysis']
